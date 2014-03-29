c0a99c063435dbfb1f25d271e9a90b2377750d4d
Hutson Betts <hutson@hyper-expanse.net>
2014-03-29 16:25:26 -0500

    Add Vim setup support to deploy script.
    Add additional step to deploy script to pull down Vundle, a Vim plugin
    manager, and then execute Vim to install all user specified plugins
    (Again, using Vundle).

--- deploy.sh
+++ deploy.sh
@@ -9,17 +9,36 @@
 echo "Deploying dotfiles..."
 
 # Symlink files to the user's home directory.
+echo "> Symlinking files into the user directory (~)."
 for file in `find . -type f -name '.*' -print`; do
 	rm ~/${file#./} &> /dev/null
 	ln -s `pwd`/${file#./} ~/${file#./}
 done
 
 # Symlink top-level directories to the user's home directory.
-echo "Symlinking directories not supported yet."
+echo "> Symlinking directories not supported yet."
 for directory  in `find . -maxdepth 1 -type d -print`; do
 	if [ "${directory}" == './.git' -o "${directory}" == '.' ]; then
 		continue
 	fi
 done
+
+# Pull down the Vundle repo into Vim's Bundle directory so that it's available for installing, and managing, Vim plugins.
+if [ "$(type git 2> /dev/null)" ]; then
+	echo "> Cloning Vundle for Vim plugin management."
+
+	# Create the initial bundle directory that will be required for storing Vim plugins.
+	mkdir -p ~/.vim/bundle/vundle
+
+	# Clone the required Vundle plugin into the newly created bundle directory.
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle 2> /dev/null
+
+	echo "> Installing Vim plugins through Vundle."
+
+	# Launch Vim to install the plugins listed in the user's .vimrc configuration file, and then make sure Vim terminates.
+	vim +PluginClean! +PluginInstall! +qa
+else
+	echo "> ERROR: `git` is required for setting up Vundle, but it's not available in your PATH. Please install `git` and ensure it's in your PATH. Then re-run the deploy script."
+fi
 
 echo "Finished deploying dotfiles."
