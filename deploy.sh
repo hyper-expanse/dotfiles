26ed554b00939210c63fcdb6d5b40eaec040728c
Hutson Betts <hutson@hyper-expanse.net>
2014-07-22 00:32:59 -0500

    Use bashrc functions in deployment script.

--- deploy.sh
+++ deploy.sh
@@ -26,22 +26,10 @@ done
 # Source the newly installed profile script to setup the user's environment.
 source ${HOME}/.profile
 
-# Pull down the Vundle repo into Vim's Bundle directory so that it's available for installing, and managing, Vim plugins.
-if [ "$(type git 2> /dev/null)" ]; then
-	echo "> Cloning Vundle for Vim plugin management."
+# Install Vim and its environment.
+setupVim
 
-	# Create the initial bundle directory that will be required for storing Vim plugins.
-	mkdir -p ~/.vim/bundle/vundle
-
-	# Clone the required Vundle plugin into the newly created bundle directory.
-	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle 2> /dev/null
-
-	echo "> Installing Vim plugins through Vundle."
-
-	# Install Vim plugins and their dependencies.
-	updateVim
-else
-	echo "> ERROR: `git` is required for setting up Vundle, but it's not available in your PATH. Please install `git` and ensure it's in your PATH. Then re-run the deploy script."
-fi
+# Install Vim plugins and their dependencies.
+updateVim
 
 echo "Finished deploying dotfiles. Your environment has been sourced and setup. Enjoy."
