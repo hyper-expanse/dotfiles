96841e8ab0ee1abc3380a3d538e9bc474e378c9f
Hutson Betts <hutson@hyper-expanse.net>
2016-11-08 21:46:39 -0600

    Add support for provisioning the user for Vagrant.

--- .vagrant.d/system-provisioner.sh
+++ .vagrant.d/system-provisioner.sh
@@ -1,3 +1,5 @@
+# Update Software
+
 echo "deb http://httpredir.debian.org/debian jessie main contrib non-free
 	deb-src http://httpredir.debian.org/debian jessie main contrib non-free
 
@@ -14,3 +16,52 @@ echo "Package: *
 aptitude update
 
 aptitude full-upgrade --assume-yes
+
+# Additional System Packages
+
+aptitude install apt-listbugs ntp sshfs
+
+# SSH Configuration
+
+echo "===========================
+HyperExpanse Network Notice
+===========================
+
+Unauthorized access to this computer system is prohibited. Users have no expectation of privacy except as otherwise provided by applicable privacy laws. All activities are monitored, logged, and analyzed. Unauthorized access and activities or any criminal activity will be reported to appropriate authorities." > /etc/issue
+
+cp /etc/issue /etc/issue.net
+
+# Security - Kernel Configuration
+
+echo "#########################
+# Kernel Settings
+#########################
+
+# Disables the magic-sysrq key
+kernel.sysrq = 0" > /etc/sysctl.d/security.conf
+
+sudo sysctl -p /etc/sysctl.d/security.conf
+
+# Security - Restricting Task Scheduling
+
+echo "root" > /etc/at.allow
+
+echo "root" > /etc/cron.allow
+
+# Security - Password Scheme
+
+aptitude install libpam-cracklib
+
+# Security - Debian Vulnerability Scanner
+
+aptitude install debsecan
+
+# Security - Update Host SSH Keys
+
+# TODO - Change to 16384 at some point.
+yes | ssh-keygen -t rsa -b 8192 -N '' -f /etc/ssh/ssh_host_rsa_key
+yes | ssh-keygen -t dsa -b 1024 -N '' -f /etc/ssh/ssh_host_dsa_key
+
+# Personal - LinuxBrew
+
+aptitude install build-essential curl git python-setuptools ruby --assume-yes
---
+++ .vagrant.d/user-provisioner.sh
@@ -0,0 +1,16 @@
+# Install dotfiles project.
+
+wget https://gitlab.com/hyper-expanse/dotfiles/repository/archive.tar.gz?ref=installation -O "/tmp/dotfiles.tar.gz"
+mkdir "${HOME}/.dotfiles"
+tar -xf "/tmp/dotfiles.tar.gz" -C "${HOME}/.dotfiles/" --strip-components=1
+rm "/tmp/dotfiles.tar.gz"
+
+# Deploy dotfiles.
+
+cd "${HOME}/.dotfiles"
+bash deploy.sh
+
+# Setup environment.
+
+source ~/.profile
+setupEnvironment
