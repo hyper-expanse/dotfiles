543941f88e8fb5efdb71d8502c031cca12149d58
Hutson Betts <hutson@hyper-expanse.net>
2016-11-15 22:57:47 -0600

    Fix improper use of quotes in deploy script documentation.

--- deploy.sh
+++ deploy.sh
@@ -57,4 +57,4 @@ echo "> Symlinking third-party scripts into the data directory (${XDG_DATA_HOME}
 mkdir --parents "${XDG_DATA_HOME}/nvim"
 ln --symbolic --force "$(pwd)/markdown2ctags/markdown2ctags.py" "${XDG_DATA_HOME}/nvim/markdown2ctags.py"
 
-echo "Finished deploying your dotfiles. Please run `source ~/.profile` to use your new setup."
+echo "Finished deploying your dotfiles. Please run 'source ~/.profile' to use your new setup."
